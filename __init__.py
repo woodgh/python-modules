78b5770c78dcd3e2f90db583603bf9725917ce15
woodgh <woodgh@users.noreply.github.com>
2020-10-21 14:09:39 +0900

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import io 
+import os
+import sys
+import json
+import github
+import logging
+import datetime
+import requests
+import logger
+import configure
+import archive
+
+''' Initialize '''
+def Initialize(path):
+    # Requests warning disabled
+    requests.packages.urllib3.disable_warnings(
+        requests.packages.urllib3.exceptions.InsecureRequestWarning
+    )
+
+    # Logger
+    logger.Logger(path)
+
+    # Configure
+    return configure.Configure(path)
+
+''' Archive '''
+def Archive(token, repo):
+    return archive.Archive(token, repo)
